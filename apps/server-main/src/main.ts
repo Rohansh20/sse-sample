9c5dcfa33619422cccfd89b5544c7bbd4ffb5880
Rohan Sharma <rohan@Rohans-MacBook-Pro.local>
2018-12-22 00:57:57 +0530

    server-main - Added CORS, /welcome route

--- apps/server-main/src/main.ts
+++ apps/server-main/src/main.ts
@@ -7,8 +7,14 @@ import * as express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`Welcome to server-main!`);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/welcome', (req, res) => {
+  res.json({ message: 'Welcome to server-main!'})
 });
 
 const port = 3333;
