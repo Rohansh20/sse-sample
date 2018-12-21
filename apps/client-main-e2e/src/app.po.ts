7891c16f1ac427b07d1b6f88206fc97819142ccc
Rohan Sharma <rohan@Rohans-MacBook-Pro.local>
2018-12-22 00:35:37 +0530

    Created client-main app

---
+++ apps/client-main-e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('sse-sample-root h1')).getText();
+  }
+}
