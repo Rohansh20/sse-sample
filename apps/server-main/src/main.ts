4a52729648d73cd5a53c8b21092cb206c512c252
Rohan Sharma <rohan@Rohans-MacBook-Pro.local>
2018-12-22 01:46:04 +0530

    server-main - Added /counter event-stream

--- apps/server-main/src/main.ts
+++ apps/server-main/src/main.ts
@@ -8,13 +8,35 @@ import * as express from 'express';
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
 app.get('/welcome', (req, res) => {
-  res.json({ message: 'Welcome to server-main!'})
+  res.json({ message: 'Welcome to server-main!' });
+});
+
+app.get('/counter', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream'
+  });
+  let counterValue = 0;
+  const interval = setInterval(() => {
+    const currentCounterValue = counterValue++;
+    const response = { counterValue: currentCounterValue };
+    res.write(
+      `id:${currentCounterValue}\nevent:counter\ndata:${JSON.stringify(
+        response
+      )}\n\n`
+    );
+  }, 1000);
+  setTimeout(() => {
+    clearInterval(interval);
+  }, 10000);
 });
 
 const port = 3333;
